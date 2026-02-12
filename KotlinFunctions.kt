77ae79fe7aed0f82f0e16103af162cfab2e43164
Andrey Veshchev <134205601+VeshchevAndrey@users.noreply.github.com>
2026-02-12 09:49:41 +0300

    Update KotlinFunctions.kt

--- KotlinFunctions.kt
+++ KotlinFunctions.kt
@@ -37,9 +37,45 @@ fun sign(x: Double): Int {
     else return 0
 }
 
+// Функция с переменным количеством параметров
+fun students(group: String, vararg students: String){
+    println("Название группы: $group\nУчащиеся:")
+    for (i in students) println(i)
+}
+
+// Функция расчёта среднего значения множества дробных (вещественных) чисел
+fun mean(vararg numbers: Double): Double{
+    var sum = 0.0
+    for (i in numbers) sum += i
+    return sum / numbers.size
+}
+
+// Функция с переменным параметром и стандартными параметрами
+fun country(name: String, vararg bigCity: String, capital: String){
+    println("Страна: $name")
+    print("Крупные города: ")
+    for (i in bigCity) print("$i, ")
+    println()
+    println("Столица: $capital")
+}
+
+fun square(vararg numbers: Int){
+    for (i in numbers) println(i * i)
+}
+
+// Однострочная функция (тело функции состоит из одного выражения)
+fun singleSquare(number: Int) = number * number
+
+// Анонимная функция
+val anonymous = fun (x: Int, y: Int): Int{
+    return x * y
+}
+
+// Лямбда-выражение
+val number = {num: Int -> println(singleSquare(num))}
 
 fun main(){
-    welcome() // вызов функции
+    // welcome() // вызов функции
     new_user("Мария", 20) // передача обязательных параметров
     employee("John", "Smith", 30, "high")
     employee("Jane", "Doe", 20)
@@ -57,4 +93,15 @@ fun main(){
     println(sign(0.0))
     println(sign(-1.4))
     println(sign(2.4) + sign(-4.2))
+    students("СУ-11", "Иванов", "Сергеев", "Александров", "Николаев")
+    val arithMean = mean(22.0, 74.0, 16.0)
+    println(arithMean)
+    country("Россия", "Санкт-Петербург", "Казань", "Екатеринбург", "Новосибирск", capital = "Москва")
+    val arrayNumbers = intArrayOf(4, 7, 16, 42)
+    square(*arrayNumbers) // * - указание распаковки массива
+    val x = singleSquare(256)
+    println(x)
+    val result = anonymous(5, 8)
+    println(result)
+    number(6)
 }
