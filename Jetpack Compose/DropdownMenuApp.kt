31e205544e5bd942bba825f2bd156104a9536882
Andrey Veshchev <134205601+VeshchevAndrey@users.noreply.github.com>
2026-03-18 12:29:20 +0300

    Create DropdownMenuApp.kt

---
+++ Jetpack Compose/DropdownMenuApp.kt
@@ -0,0 +1,169 @@
+// package com.example.application247
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Refresh
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        // Отрисовка элементов интерфейса на экране приложения
+        setContent {
+            val selectedText = remember { mutableStateOf("") }
+            Scaffold(
+                topBar = { MyTopBar(selectedText) },
+                bottomBar = { MyBottomBar(selectedText) }
+            ) { Application5(modifier = Modifier.padding(it), selectedText) }
+        }
+    }
+}
+
+@Composable
+fun MyDropdownMenu(text: MutableState<String>){
+    val dropdownMenuState = remember { mutableStateOf(false) }
+    val alertDialogState = remember { mutableStateOf(false) }
+
+    IconButton(onClick = {dropdownMenuState.value = true}) {
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "More",
+            tint = Color(0xFFEDE7F6)
+        )
+    }
+    DropdownMenu(
+        expanded = dropdownMenuState.value,
+        onDismissRequest = { dropdownMenuState.value = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text(text = "Обновить") },
+            onClick = {text.value = ""},
+            leadingIcon = { Icon(Icons.Rounded.Refresh, "Refresh") }
+        )
+        HorizontalDivider(thickness = 2.dp)
+        DropdownMenuItem(
+            text = { Text(text = "О создателе") },
+            onClick = {alertDialogState.value = true},
+            leadingIcon = { Icon(Icons.Rounded.Info, "About") }
+        )
+    }
+
+    if (alertDialogState.value) {
+        AlertDialog(
+            onDismissRequest = { alertDialogState.value = false },
+            title = { Text(text = "О создателе") },
+            text = { Text(text = "Создал Я :)") },
+            confirmButton = { Button(
+                onClick = {alertDialogState.value = false}
+            ) { Text(text = "Закрыть") } }
+        )
+    }
+
+}
+
+@Composable
+fun MyBottomBar(text: MutableState<String>){
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        IconButton(onClick = {text.value = "Home"}) {
+            Icon(
+                imageVector = Icons.Rounded.Home,
+                contentDescription = "Home"
+            )
+        }
+        IconButton(onClick = {text.value = "Phone"}) {
+            Icon(
+                imageVector = Icons.Rounded.Phone,
+                contentDescription = "Phone"
+            )
+        }
+        IconButton(onClick = {text.value = "Favorite"}) {
+            Icon(
+                imageVector = Icons.Rounded.Favorite,
+                contentDescription = "Favorite"
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(text: MutableState<String>){
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name)) },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFFF57F17)),
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Rounded.Menu,
+                    contentDescription = "Menu",
+                    tint = Color(0xFFEDE7F6)
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.night),
+                    contentDescription = "Dark Theme",
+                    tint = Color(0xFFEDE7F6)
+                )
+            }
+            MyDropdownMenu(text)
+        }
+    )
+}
+
+@Composable
+fun Application5(modifier: Modifier = Modifier, text: MutableState<String>){
+    Column(modifier = modifier) {
+        Text(text = text.value)
+    }
+
+}
+
+@Preview(showBackground = true) // аннотация для предпросмотра Composable-функций
+@Composable
+fun PreviewForMyFunctions(){
+
+}
